2181ad99723aa156abdf1a492fc8d6fdfa220e52
Ed <edwoulfe@gmail.com>
2019-12-20 11:10:57 -0600

    Special methods and overriding built in methods from the python library

---
+++ special-methods.py
@@ -0,0 +1,29 @@
+class Employee:
+
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + "." + last + "@company.com"
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+    def __repr__(self):
+        return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
+
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)
+
+
+emp_1 = Employee("Frank", "Tesla", 50000)
+emp_2 = Employee("Jack", "Rank", 70000)
+
+
+print(repr(emp_1))
+print(str(emp_1))
